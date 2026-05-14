7b7fa09537deda2c278b71de30726afff8ea5ad4
communetayo <tayocommune@gmail.com>
2026-05-14 18:05:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import requests
 import tempfile
 import os
 import re
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 app = FastAPI(title="V-Extractor", version="1.0.0")
 
